8815daf73cae812eb057a7a6cff75d6779bed826
VickRobin <a.22308051281326@cbtis128.edu.mx>
2025-02-26 20:45:51 +0000

    img

--- lib/main.dart
+++ lib/main.dart
@@ -11,39 +11,96 @@ class MyIcons extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-            title: Text("Roberto Vazquez"),
-            titleTextStyle: TextStyle(color: Colors.white, fontSize: 20),
-            centerTitle: true,
-            backgroundColor: Colors.deepPurple),
+          title: Text("Roberto Vazquez"),
+          titleTextStyle: TextStyle(color: Colors.white, fontSize: 20),
+          centerTitle: true,
+          backgroundColor: Colors.deepPurple,
+        ),
         body: Column(
           children: <Widget>[
+            // Título con nombre y número
             Text(
               "Roberto Vazquez 22308051281326",
               style: TextStyle(fontSize: 20, color: Colors.black),
             ),
-            const SizedBox(
-              height: 50,
-            ),
+            const SizedBox(height: 50),
+
+            // Primera fila de iconos con subtítulos
             Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  Icon(
-                    Icons.remove_red_eye,
-                    color: Colors.cyan,
-                    size: 50,
-                  ),
-                  Icon(
-                    Icons.theater_comedy,
-                    color: Colors.red,
-                    size: 50,
-                  ),
-                  Icon(Icons.heart_broken, color: Color(0xffff5bff), size: 50),
-                  Icon(
-                    Icons.star_half,
-                    color: Colors.deepPurpleAccent,
-                    size: 50,
-                  )
-                ])
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                Column(
+                  children: [
+                    Icon(
+                      Icons.home,
+                      color: Colors.black,
+                      size: 50,
+                    ),
+                    Text("Home"),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Icon(
+                      Icons.theater_comedy,
+                      color: Colors.red,
+                      size: 50,
+                    ),
+                    Text("Masks"),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Icon(
+                      Icons.heart_broken,
+                      color: Color(0xffff5bff),
+                      size: 50,
+                    ),
+                    Text("Heartbreaker"),
+                  ],
+                ),
+              ],
+            ),
+
+            // Espacio entre las filas
+            SizedBox(height: 40),
+
+            // Segunda fila de iconos con subtítulos
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Column(
+                  children: [
+                    Icon(
+                      Icons.card_membership,
+                      color: Color(0xffa89701),
+                      size: 50,
+                    ),
+                    Text("Membership"),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Icon(
+                      Icons.games_sharp,
+                      color: Colors.grey,
+                      size: 50,
+                    ),
+                    Text("D-Pad"),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Icon(
+                      Icons.face_6,
+                      color: Color(0xff007bff),
+                      size: 50,
+                    ),
+                    Text("Men"),
+                  ],
+                ),
+              ],
+            ),
           ],
         ),
       ),
